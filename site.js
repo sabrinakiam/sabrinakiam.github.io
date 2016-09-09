02464f7e35fd6ef60429421677f744e761eee0f0
sabrina <kiamilev@usc.edu>
2016-09-09 01:47:25 -0700

    tinker tinker

--- site.js
+++ site.js
@@ -1,6 +1,7 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
